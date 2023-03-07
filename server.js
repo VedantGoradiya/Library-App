c77746ab43839b49c4b619a8106c36b36156e0d2
Vedant Goradiya <vedantgoradiya1998@gmail.com>
2023-03-07 16:53:26 -0500

    First commit to Library app build using NodeJs and Express

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(methodOverride('_method'))
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
+const mongoose = require('mongoose')
+mongoose.connect('Include your URL', 
+   { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.on('open', () => console.log('Connected'))
+
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
+
+app.listen(3000)
